61dce11615871252211bd60d83ebb53c8ec17b4c
qltes <118480096+qltes@users.noreply.github.com>
2023-09-01 22:58:48 -0230

    Add files via upload

---
+++ Plugin.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using BepInEx;
+using UnityEngine;
+using Utilla;
+
+namespace GorillaTagModTemplateProject
+{
+
+	[BepInDependency("org.legoandmars.gorillatag.utilla", "1.5.0")]
+	[BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+	public class Plugin : BaseUnityPlugin
+	{
+		GameObject mirror;
+		GameObject mirrorBoard;
+		GameObject mirrorStand;
+
+		void Start()
+		{
+			Utilla.Events.GameInitialized += OnGameInitialized;
+		}
+
+		void OnGameInitialized(object sender, EventArgs e)
+		{
+			mirror = GameObject.Find("TreeRoom/TreeRoomInteractables/mirror (1)");
+			mirrorBoard = GameObject.Find("TreeRoom/TreeRoomInteractables/mirror (1)/board");
+			mirrorStand = GameObject.Find("TreeRoom/TreeRoomInteractables/mirror (1)/stand");
+
+		}
+
+		void Update()
+		{
+			mirror.SetActive(this.enabled);
+			mirror.GetComponentInChildren<MeshCollider>().enabled = false;
+			mirrorBoard.GetComponent<MeshCollider>().enabled = false;
+			mirrorStand.GetComponent<MeshCollider>().enabled = false;
+		}
+	}
+}
---
+++ PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace GorillaTagModTemplateProject
+{
+	/// <summary>
+	/// This class is used to provide information about your mod to BepInEx.
+	/// </summary>
+	class PluginInfo
+	{
+		public const string GUID = "com.qltes.gorillatag.oldmirrorfix";
+		public const string Name = "OldMirrorFix";
+		public const string Version = "1.0.0";
+	}
+}
